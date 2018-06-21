134cbf2772b3c1db2dfeee3e9204f3afb7c830df
Sinevia <info@sinevia.com>
2018-06-21 05:21:05 +0100

    Update shop.php

--- config/shop.php
+++ config/shop.php
@@ -1,1 +1,19 @@
 <?php
+
+/*
+ * Set specific configuration variables here
+ */
+return [
+    // automatic loading of routes through main service provider
+    'routes' => true,
+    // layout where the Shop will show into, i.e. admin.layouts.master
+    'layout-master' => 'shop::admin.layout',
+    // URLs
+    'urls' => [
+        'media-manager' => '#/your-media-manager-url',
+    ],
+    'paths' => [
+        // path to widgets, in your resources directory
+        //'widgets' => 'widgets',
+    ],
+];
